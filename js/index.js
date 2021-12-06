d93177f4acc80a0047e1a78cd0cea60ab52dd585
nas1bas <nasiba4647@gmail.com>
2021-12-06 06:35:07 +0300

    change files

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-var toggleButton = document.querySelector (".header__btn")
-var siteNavbar = document.querySelector (".sitenav")
+// var toggleButton = document.querySelector (".header__btn")
+// var siteNavbar = document.querySelector (".sitenav")
 
-toggleButton.addEventListener("click", () =>{
-    siteNavbar.classList.toggle("sitenav-open");
-    toggleButton.classList.toggle("close-btn");
-});
+// toggleButton.addEventListener("click", () =>{
+//     siteNavbar.classList.toggle("sitenav-open");
+//     toggleButton.classList.toggle("close-btn");
+// });
