ffe598e01ecd4026df603742013216531cce3894
nas1bas <nasiba4647@gmail.com>
2021-12-11 15:17:29 +0300

    edit files

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-// var toggleButton = document.querySelector (".header__btn")
-// var siteNavbar = document.querySelector (".sitenav")
 
-// toggleButton.addEventListener("click", () =>{
-//     siteNavbar.classList.toggle("sitenav-open");
-//     toggleButton.classList.toggle("close-btn");
-// });
+
+var openBtn = document .querySelector ('.header__btn')
+var closeBtn = document.querySelector ('.menu')
+
+openBtn.addEventListener ("click",function () {
+    menu.classList.add('menu-open')
+})
+
+closeBtn.addEventListener ("click", function () {
+    menu.classList.remove('menu-open')
+}) 
+    
